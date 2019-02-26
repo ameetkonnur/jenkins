a95fc22562e3ab0751c817034ff139e4d8505922
Ameet Konnur <ameetk@live.in>
2019-02-26 11:14:05 +0530

    m

--- quickstart_templates/zero_downtime_deployment/301-jenkins-vmss-zero-downtime-deployment.sh
+++ quickstart_templates/zero_downtime_deployment/301-jenkins-vmss-zero-downtime-deployment.sh
@@ -45,9 +45,12 @@ function run_util_script() {
 
 function install_az() {
   if !(command -v az >/dev/null); then
+    sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
     sudo apt-get update && sudo apt-get install -y libssl-dev libffi-dev python-dev
-    echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-    sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+    #echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+    sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
     sudo apt-get install -y apt-transport-https
     sudo apt-get -y update && sudo apt-get install -y azure-cli
   fi
